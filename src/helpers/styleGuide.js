b0389f141300188d24063135928f9a536e94a827
Vlad <00cmb.ua@gmail.com>
2024-01-12 01:53:33 +0200

    css edit

---
+++ src/helpers/styleGuide.js
@@ -0,0 +1,16 @@
+// Базові кольори і переходи
+// Приклад використання в '../components/Button/Button.styled.jsx'
+
+export const styleGuide = {
+  blackColor: '#040404',
+  orangeColor: '#e6533c',
+  orange2Color: '#ef8964',
+  beigeColor: '#efa082',
+  whiteColor: ' #efede8',
+  successColor: '#3cbf61',
+  errorColor: '#d80027',
+  modalBackgroundColor: 'rgba(4, 4, 4, 0.4)',
+
+  modalTransition: 'scale 300ms linear',
+  animation: '250ms cubic-bezier(0.23, 0.86, 0.79, 0.77)',
+};
