13f0aec1f09cd507b17bea41325328928476f1e1
SaltyUA <104589666+SaltyUA@users.noreply.github.com>
2024-01-12 18:50:11 +0200

    buttons change

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,7 +1,18 @@
-import { FullfilledButton } from './Button.styled';
+import { StyledButton } from './Button.styled';
 
-const Button = ({ title, to }) => {
-  return <FullfilledButton to={to}>{title}</FullfilledButton>;
+const Button = ({ children, transparent, width, onSubmit, as, type, to }) => {
+  return (
+    <StyledButton
+      type={type}
+      as={as}
+      to={to}
+      onSubmit={onSubmit}
+      $width={width}
+      $transparent={transparent}
+    >
+      {children}
+    </StyledButton>
+  );
 };
 
 export default Button;
