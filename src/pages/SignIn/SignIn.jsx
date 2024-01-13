3dd167446f41b484e19db2ad836b5b27629ecc25
Vlad <00cmb.ua@gmail.com>
2024-01-13 16:38:04 +0200

    login validating

--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -3,25 +3,46 @@ import AuthInput from '../../components/AuthInput/AuthInput';
 import { emailPattern } from '../../constants/patterns';
 import { FormContainer, FormTitle, FormWrapper } from './SignIn.styled';
 import { Link } from 'react-router-dom';
+import { useFormik } from 'formik';
+import { object, string } from 'yup';
 
 const SignIn = () => {
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+    onSubmit: (values) => {
+      console.log('SignUp', values);
+    },
+    validationSchema: object().shape({
+      email: string()
+        .required('Please enter email')
+        .matches(emailPattern, 'Please check is it true email'),
+    }),
+    password: string().required('Please enter password'),
+  });
+
   return (
     <FormContainer>
       <FormTitle>Sign In</FormTitle>
       <p>Welcome! Please enter your credentials to login to the platform:</p>
-      <FormWrapper>
+      <FormWrapper onSubmit={formik.handleSubmit}>
         <AuthInput
+          value={formik.values.email}
+          onChange={formik.handleChange}
           placeholder="Email"
           name="email"
           type="email"
-          pattern={emailPattern}
         />
-        <AuthInput placeholder="Password" name="password" type="password" />
-        <Button
-          type="submit"
-          width="136px"
-          // onSubmit={handleSubmit}
-        >
+        <AuthInput
+          value={formik.values.password}
+          onChange={formik.handleChange}
+          placeholder="Password"
+          name="password"
+          type="password"
+        />
+        <Button type="submit" width="136px">
           SignIn
         </Button>
         <p>
