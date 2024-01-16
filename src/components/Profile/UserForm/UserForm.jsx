3b3364c546266fc68fe18b62a638158c2e952a7a
Mykhailo Popyzenko <mp.and.sp@ukr.net>
2024-01-16 23:33:54 +0200

    updated validation scheme; added markup for the form;

--- src/components/Profile/UserForm/UserForm.jsx
+++ src/components/Profile/UserForm/UserForm.jsx
@@ -1,27 +1,30 @@
-import { useEffect } from 'react';
-import { Formik, Field, Form } from 'formik';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { updateUserInfo } from 'redux/operations';
+
+import { Formik, Form } from 'formik';
 import * as Yup from 'yup';
 
 const userFormSchema = Yup.object().shape({
   name: Yup.string()
     .min(2, 'Too Short!')
     .max(50, 'Too Long!')
-    .required('Required'),
+    .required('Name is required'),
   height: Yup.number()
-    .positive('Must be a positive number')
+    .positive('Height must be a positive number')
     .min(150, 'Must be a number from 150 to 300')
     .max(300, 'Must be a number from 150 to 300')
     .required('Height is required'),
   currentWeight: Yup.number()
-    .positive('Must be a positive number')
-    .min(35, 'Must be a number from 35 to 500')
-    .max(500, 'Must be a number from 35 to 500')
-    .required('Weight is required'),
+    .positive('Weight must be a positive number')
+    .min(35, 'Weight must be a number from 35 to 500')
+    .max(500, 'Weight must be a number from 35 to 500')
+    .required('Current weight is required'),
   desiredWeight: Yup.number()
-    .positive('Must be a positive number')
-    .min(35, 'Must be a number from 35 to 500')
-    .max(500, 'Must be a number from 35 to 500')
-    .required('Weight is required'),
+    .positive('Weight must be a positive number')
+    .min(35, 'Weight must be a number from 35 to 500')
+    .max(500, 'Weight must be a number from 35 to 500')
+    .required('Desired weight is required'),
   birthday: Yup.date()
     .min(
       new Date(new Date().setFullYear(new Date().getFullYear() - 18)),
@@ -40,18 +43,91 @@ const userFormSchema = Yup.object().shape({
 });
 
 const UserForm = () => {
-//   const initialValues = {
-//     name: user.name ?? '',
-//     height: user.height ?? 150,
-//     currentWeight: user.currentWeight ?? 70,
-//     desiredWeight: user.desiredWeight ?? 60,
-//     birthday: user.birthday ?? '1990-01-01',
-//     blood: user.blood ?? 1,
-//     sex: user.sex ?? 'male',
-//     levelActivity: user.levelActivity ?? 1,
-//   };
+  const dispatch = useDispatch();
+  const user = useSelector();
 
-  return <Formik></Formik>;
+  const initialValues = {
+    name: user.name ?? '',
+    height: user.height ?? 150,
+    currentWeight: user.currentWeight ?? 35,
+    desiredWeight: user.desiredWeight ?? 35,
+    birthday: user.birthday ?? '1990-01-01',
+    blood: user.blood ?? 1,
+    sex: user.sex ?? 'male',
+    levelActivity: user.levelActivity ?? 1,
+  };
+
+  const handleSubmit = (values) => {
+    dispatch(updateUserInfo(values)); // updateUserInfo - майбутня фукнція в redux/operations
+  };
+
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={userFormSchema}
+      onSubmit={handleSubmit}
+    >
+      {({ handleChange, values }) => (
+        <Form>
+          <label>
+            Name
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={values.name}
+              onChange={handleChange}
+            />
+          </label>
+          <label>
+            Email
+            <input
+              type="text"
+              id="email"
+              name="email"
+              defaultValue={user.email}
+              readOnly
+              disabled
+            />
+          </label>
+          <label>
+            Height
+            <input
+              type="number"
+              id="height"
+              name="height"
+              value={values.height}
+              onChange={handleChange}
+            />
+          </label>
+          <label>
+            Current Weight
+            <input
+              type="number"
+              id="currentWeight"
+              name="currentWeight"
+              value={values.currentWeight}
+              onChange={handleChange}
+            />
+          </label>
+          <label>
+            Desired Weight
+            <input
+              type="number"
+              id="desiredWeight"
+              name="desiredWeight"
+              value={values.desiredWeight}
+              onChange={handleChange}
+            />
+          </label>
+          <label>
+            Date of birth
+            <input type="date" />
+          </label>
+        </Form>
+      )}
+    </Formik>
+  );
 };
 
 export default UserForm;
