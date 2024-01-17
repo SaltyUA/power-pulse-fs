36a3865a71dff2a72451235ba0377200b23e3224
Mykhailo Popyzenko <mp.and.sp@ukr.net>
2024-01-17 20:18:55 +0200

    markup for UserCard is ready

--- src/components/Profile/UserCard/UserCard.jsx
+++ src/components/Profile/UserCard/UserCard.jsx
@@ -1,34 +1,107 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
-import { selectUser } from 'redux/selectors';
-import { updateAvatar } from 'redux/operations';
+// import { selectUser } from 'redux/selectors';
+// import { updateAvatar } from 'redux/operations';
+import { logOut } from '../../../store/auth/thunk';
 
 const UserCard = () => {
+  const avatarURL = '';
   const dispatch = useDispatch();
-  const user = useSelector(selectUser);
+  // const user = useSelector(selectUser);
 
-  const [avatarURL, setAvatarURL] = useState(user.avatar);
+  // const [avatarURL, setAvatarURL] = useState(user.avatar);
 
-//   const handleIconClick = () => {
-//     if (fileInputRef.current) {
-//       fileInputRef.current.click();
-//     }
-//   };
+  const fileInputRef = useRef(null);
 
-  const handleAvatarChange = (event) => {
-      setAvatarURL(URL.createObjectURL(event.currentTarget.files[0]));
-      
-      try {
-        dispatch(updateAvatar())
-      } catch (error) {
-        
-      }
+  const handleIconClick = () => {
+    console.log();
+    // if (fileInputRef.current) {
+    //   fileInputRef.current.click();
+    // }
   };
 
+  const handleAvatarChange = (event) => {
+    console.log(event);
+    //   const file = event.currentTarget.files[0];
+
+    //   if (file) {
+    //     setAvatarURL(URL.createObjectURL(file));
+    //   }
+
+    //   try {
+    //     dispatch(updateAvatar(file));
+    //   } catch (error) {
+    //     console.log(error);
+    //   }
+  };
+
+  const handleLogOut = () => dispatch(logOut());
+
   return (
-    <div>
-      <p></p>
+    <div className="containerSection">
+      <div className="containerAvatar">
+        {avatarURL ? (
+          <img src={avatarURL} alt="User Avatar" />
+        ) : (
+          <svg>
+            <use href=""></use>
+          </svg>
+        )}
+      </div>
+
+      <form>
+        <input
+          type="file"
+          name="avatarURL"
+          accept="image/*"
+          ref={fileInputRef}
+          onChange={handleAvatarChange}
+        />
+        <div className="icon-upload" onClick={handleIconClick}>
+          <svg>
+            <use href=""></use>
+          </svg>
+        </div>
+      </form>
+
+      {/* <p>{user.name}</p> */}
+      <p>Name</p>
+      <p>User</p>
+
+      <div className="containerStatistic">
+        <div className="daily_calorie">
+          <svg>
+            <use></use>
+          </svg>
+          <p>Daily calorie intake</p>
+          <p>100</p>
+        </div>
+        <div className="daily_physical">
+          <svg>
+            <use></use>
+          </svg>
+          <p>Daily physical activity</p>
+          <p>110 min</p>
+        </div>
+      </div>
+
+      <div className="text_under_statistic">
+        <svg>
+          <use></use>
+        </svg>
+        <p>
+          We understand that each individual is unique, so the entire approach
+          to diet is relative and tailored to your unique body and goals.
+        </p>
+      </div>
+
+      <button onClick={handleLogOut} style={{ color: 'white' }}>
+        <p>Log out</p>
+        <svg>
+          <use></use>
+        </svg>
+      </button>
     </div>
   );
 };
