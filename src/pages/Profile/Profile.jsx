88fb47ce8e73d0523a40c345f21784137beaa3cb
SKhrebtan <shrebtan@gmail.com>
2024-01-19 01:51:20 +0200

    CHANGE STORE add page animation and error handler

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,14 +1,17 @@
 import { TitlePage } from './Profile.styled';
 import UserCard  from '../../components/Profile/UserCard';
-import UserForm  from '../../components/Profile/UserForm';
+import UserForm from '../../components/Profile/UserForm';
+import { PageAnimatedWrapper } from '../../components/AnimatedPage/PageAnimatedWrapper';
 
 const Profile = () => {
   return (
+    <PageAnimatedWrapper direction='X'>
     <div>
       <TitlePage>Profile Settings</TitlePage>
       <UserCard />
       <UserForm />
     </div>
+    </PageAnimatedWrapper>
   );
 };
 
