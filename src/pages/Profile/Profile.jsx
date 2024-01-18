1dbc88c5d881c260283646540a03d3ec45dc83e1
Stereotype89 <sh.alexandr.1989@gmail.com>
2024-01-18 12:24:53 +0100

    add-adaptiv

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,14 +1,22 @@
 import { TitlePage } from './Profile.styled';
-import UserCard  from '../../components/Profile/UserCard';
-import UserForm  from '../../components/Profile/UserForm';
+import UserCard from '../../components/Profile/UserCard';
+import UserForm from '../../components/Profile/UserForm';
+import { ContainerWrapper } from '../../components/Container/Container.styled';
+import { ProfilerWrapper, WrapperContent, WrapperForm } from './Profile.styled';
 
 const Profile = () => {
   return (
-    <div>
-      <TitlePage>Profile Settings</TitlePage>
-      <UserCard />
-      <UserForm />
-    </div>
+    <ContainerWrapper>
+      <ProfilerWrapper>
+        <TitlePage>Profile Settings</TitlePage>
+        <WrapperContent>
+          <UserCard />
+          <WrapperForm>
+            <UserForm />
+          </WrapperForm>
+        </WrapperContent>
+      </ProfilerWrapper>
+    </ContainerWrapper>
   );
 };
 
