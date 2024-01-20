e6140ee1729ac8e31f46ebb25e441d8de1b1319c
SKhrebtan <shrebtan@gmail.com>
2024-01-20 12:30:55 +0200

    add addbtn

---
+++ src/components/AddButton/AddButton.jsx
@@ -0,0 +1,18 @@
+// import {
+//   StyledAddBtn,
+//   StyledBtnSpan,
+//   StyledArrowSvg23
+// } from './AddButton.styled';
+// import sprite from '../../assets/images/sprite.svg';
+// export const AddButton = () => {
+
+//     return (
+//          <StyledAddBtn type="button">
+//           <StyledBtnSpan className="add-btn-span">Add</StyledBtnSpan>
+//           <StyledArrowSvg23>
+//             <use href={sprite + '#locationarrow'}></use>
+//           </StyledArrowSvg23>
+         
+//         </StyledAddBtn>
+//     )
+// }
