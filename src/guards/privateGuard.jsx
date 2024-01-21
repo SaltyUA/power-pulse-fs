a27f6bb4c56568cd5e5b641171ead5c5f930fb6d
SaltyUA <104589666+SaltyUA@users.noreply.github.com>
2024-01-21 19:14:07 +0200

    fix

--- src/guards/privateGuard.jsx
+++ src/guards/privateGuard.jsx
@@ -1,12 +1,16 @@
 import { useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
-import { selectIsLoggedIn, selectIsRefreshing } from '../store/selectors';
+import {
+  selectIsLoggedIn,
+  selectIsRefreshing,
+  selectToken,
+} from '../store/selectors';
 
 export const PrivateGuard = ({ component: Component, redirectTo = '/' }) => {
   const isLoggedIn = useSelector(selectIsLoggedIn);
   const isRefreshing = useSelector(selectIsRefreshing);
-  
-  const shouldRedirect = !isLoggedIn && !isRefreshing;
+  const token = useSelector(selectToken);
+  const shouldRedirect = !isLoggedIn && !isRefreshing && !token;
 
   return shouldRedirect ? <Navigate to={redirectTo} /> : Component;
 };
